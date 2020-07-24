c61609f74252f55543a4055c2ea45aaff0ef6563
JacobLayton <jlayjlay3@gmail.com>
2020-07-24 16:17:01 -0700

    New model created

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ var Song = Backbone.Model.extend({
   initialize: function () {
     console.log("A new song has been created.");
   },
+  defaults: {
+    genre: "Hiphop",
+  },
 });
 
-var song = new Song();
+var song = new Song({
+  title: "Forest Whittaker",
+  artist: "Borther Ali",
+  publishYear: 2003,
+});

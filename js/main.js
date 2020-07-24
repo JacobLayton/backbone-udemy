70cfa5cf7aac4e1bd99885c7c8d809cc9acde27f
JacobLayton <jlayjlay3@gmail.com>
2020-07-24 16:57:25 -0700

    Created submodel

--- js/main.js
+++ js/main.js
@@ -2,13 +2,33 @@ var Song = Backbone.Model.extend({
   initialize: function () {
     console.log("A new song has been created.");
   },
+  validate: function (attrs) {
+    if (!attrs.title) {
+      return "Title is required";
+    }
+  },
   defaults: {
     genre: "Hiphop",
+  },
+  playSong: function () {
+    console.log("Song is now playing...");
   },
 });
 
 var song = new Song({
-  title: "Forest Whittaker",
   artist: "Borther Ali",
   publishYear: 2003,
 });
+
+var Instrumental = Song.extend({
+  playSong: function () {
+    // This line of code allows you to utilize a method form the base model even tho it is being overwritten in the submodel
+    // Song.prototype.playSong.apply(this);
+
+    console.log("Instrumental song is now playing...");
+  },
+});
+
+var instrumental = new Instrumental();
+
+instrumental.playSong();

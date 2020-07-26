625c3a076e8fbe7359df5510650f4a4c8a070c9b
JacobLayton <jlayjlay3@gmail.com>
2020-07-25 21:55:05 -0700

    First collection created

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Initial Model
 var Song = Backbone.Model.extend({
   initialize: function () {
     console.log("A new song has been created.");
@@ -15,11 +16,7 @@ var Song = Backbone.Model.extend({
   },
 });
 
-var song = new Song({
-  artist: "Borther Ali",
-  publishYear: 2003,
-});
-
+// Extended sub-model
 var Instrumental = Song.extend({
   playSong: function () {
     // This line of code allows you to utilize a method form the base model even tho it is being overwritten in the submodel
@@ -29,6 +26,28 @@ var Instrumental = Song.extend({
   },
 });
 
+// Instantiate new Song model called song
+var song = new Song({
+  artist: "Borther Ali",
+  publishYear: 2003,
+});
+
+// Instantiate new Instrumental submodel called instrumental
 var instrumental = new Instrumental();
 
+// Calls thhe playSong() method of instrumental
 instrumental.playSong();
+
+//======================Collections=======================//
+
+var Songs = Backbone.Collection.extend({
+  model: Song,
+});
+
+var songs = new Songs([
+  new Song({ title: "Song 1" }),
+  new Song({ title: "Song 2" }),
+  new Song({ title: "Song 3" }),
+]);
+
+songs.add(new Song({ title: "Song 4" }));
